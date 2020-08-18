a37d9572838b55e8904ed1df447a5a7184fa1e56
yokoe9073 <yokoe9073@gmail.com>
2020-08-18 12:26:41 +0900

    マイグレーションファイル作成
    image(商品画像)カラム型はstringではない気がする

--- db/migrate/20200818024052_create_items.rb
+++ db/migrate/20200818024052_create_items.rb
@@ -1,8 +1,21 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name
+      t.string :image
+      t.integer :category
+      t.string :details
+      t.integer :price 
+      t.integer :status
+      t.integer :delivery_fee
+      t.integer :exhibitor_prefectures
+      t.integer :shipping_date
+      t.references :user, foreign_key: true 
       t.timestamps
+       ### Association
+      # - belongs_to :user
+      # - has_many :comments
+      # - has_one :buyer
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_020911) do
+ActiveRecord::Schema.define(version: 2020_08_18_024052) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.integer "category"
+    t.string "details"
+    t.integer "price"
+    t.integer "status"
+    t.integer "delivery_fee"
+    t.integer "exhibitor_prefectures"
+    t.integer "shipping_date"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_08_13_020911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
