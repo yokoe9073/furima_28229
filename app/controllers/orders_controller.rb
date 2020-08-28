d770add46c56093365a681abecfe44c01413729b
yokoe9073 <yokoe9073@gmail.com>
2020-08-28 18:33:04 +0900

    クレカ決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,28 @@ class OrdersController < ApplicationController
  end
  
  def create
+  @order = Order.new(price: order_params[:price])
+  if @order.valid?
+    pay_item
+    @order.save
+    return redirect_to root_path
+  else
+    render 'index'
+  end
+end
+
+ private
+
+ def order_params
+  params.permit(:price, :token)
  end
  
+ def pay_item
+  Payjp.api_key = "sk_test_02bcdb94d05f37727e47a6c1"  # PAY.JPテスト秘密鍵
+  Payjp::Charge.create(
+    amount: order_params[:price],  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency:'jpy'                 # 通貨の種類(日本円)
+  )
+end
 end
